5c6b628ff76e0855dd7ddefff54ef4ff7e76ecc5
TomasKovyKovacik <tomas_kovacik@stuba.sk>
2022-03-09 15:30:07 +0100

    ukazka protected modifikatoru

--- src/main/java/sk/stuba/fei/uim/oop/Main.java
+++ src/main/java/sk/stuba/fei/uim/oop/Main.java
@@ -2,6 +2,11 @@ package sk.stuba.fei.uim.oop;
 
 public class Main {
     public static void main(String[] args) {
+        Test test = new Test();
+        test.tri = 4;
 
+        do {
+
+        } while (test.tri == 3);
     }
 }
---
+++ src/main/java/sk/stuba/fei/uim/oop/Test.java
@@ -0,0 +1,8 @@
+package sk.stuba.fei.uim.oop;
+
+public class Test {
+    public int jeden;
+    int dva;
+    protected int tri;
+    private int styri;
+}
