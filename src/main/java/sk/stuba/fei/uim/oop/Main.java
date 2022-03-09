ff39967787aee240aa6e7adb30b1753d4fac1fb7
TomasKovyKovacik <tomas_kovacik@stuba.sk>
2022-03-09 16:52:17 +0100

    kolekcie

--- src/main/java/sk/stuba/fei/uim/oop/Main.java
+++ src/main/java/sk/stuba/fei/uim/oop/Main.java
@@ -2,14 +2,43 @@ package sk.stuba.fei.uim.oop;
 
 import sk.stuba.fei.uim.oop.zvierata.Macka;
 import sk.stuba.fei.uim.oop.zvierata.Pes;
+import sk.stuba.fei.uim.oop.zvierata.Zviera;
+
+import java.util.ArrayList;
+import java.util.Collections;
 
 public class Main {
 
     public static void main(String[] args) {
-        var dunco = new Pes("dunco");
-        var garfield = new Macka("Garfield");
+        ArrayList<Pes> list = new ArrayList<>();
+        list.add(new Pes("dunco"));
+        list.add(new Pes("luna"));
+        System.out.println(list.size());
 
-        dunco.pohladkat();
-        garfield.pohladkat();
+        ArrayList<Macka> listMaciek = new ArrayList<>();
+        listMaciek.add(new Macka("murko"));
+        listMaciek.add(new Macka("garfield"));
+        System.out.println(listMaciek.size());
+
+        ArrayList<Zviera> listZveri = new ArrayList<>();
+        listZveri.addAll(list);
+        listZveri.addAll(listMaciek);
+        System.out.println(listZveri.size());
+
+        Collections.shuffle(listZveri);
+
+        for (var i = 0; i < listZveri.size(); i++) {
+            ((Zviera) listZveri.get(i)).pohladkat();
+        }
+
+        for (var zver : listZveri) {
+                zver.pohladkat();
+        }
+
+        var iterator = listZveri.iterator();
+        while (iterator.hasNext()) {
+            var prvok = iterator.next();
+            prvok.zvukZvierata();
+        }
     }
 }
